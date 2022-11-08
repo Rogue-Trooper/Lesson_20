6e60ec2f0931d1a7653b0ab9b54077e33d07b2c7
ivanvikvik <ivanvikvik@gmail.com>
2022-11-08 22:01:41 +0300

    Using modules and packages

---
+++ some_module.py
@@ -0,0 +1,19 @@
+__all__ = ["A", "test_a"]
+
+a = 10
+b = 20
+
+def test_a():
+    print("test_a from modele some")
+
+
+def test_b():
+    print("test_b")
+
+
+class A:
+    pass
+
+
+class B:
+    pass
---
+++ test_module.py
@@ -0,0 +1,16 @@
+# 1)
+import some_module
+
+# 2)
+# from some_module import __a, __b, A, B
+
+# 3)
+# from some2 import *
+# from some1 import test_a
+# from some_module import *
+#
+# test_a()
+
+from model import *
+
+
